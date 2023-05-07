55c59e408cce14e96191c6a2048b918d1436b4fe
Akiko Takano <akiko.pusu@gmail.com>
2023-05-07 14:06:45 +0900

    Confirmed "How to Work with Single Checkbox" section.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
+import { useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleOnChange = () => {
+    setIsChecked(!isChecked);
+  };
+
   return (
     <div className="App">
       Select your pizza topping:
       <div className="topping">
-        <input type="checkbox" id="topping" name="topping" 
-        value="Paneer" />
+        <input
+          type="checkbox"
+          id="topping"
+          name="topping"
+          checked={isChecked}
+          onChange={handleOnChange}
+          value="Paneer" />
         Paneer
+      </div>
+      <div className="result">
+        Above checkbox is {isChecked ? "checked" : "unchecked"}.
       </div>
     </div>
   );
