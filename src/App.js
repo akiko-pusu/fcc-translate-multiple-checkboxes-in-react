ca232244998c5933e454667c3f360d6cd3c9ad21
Akiko Takano <akiko.pusu@gmail.com>
2023-05-07 11:55:16 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      Select your pizza topping:
+      <div className="topping">
+        <input type="checkbox" id="topping" name="topping" 
+        value="Paneer" />
+        Paneer
+      </div>
+    </div>
+  );
+}
