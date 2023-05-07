42dfb57f859f677c892263684df514d0624f3e34
Akiko Takano <akiko.pusu@gmail.com>
2023-05-07 21:48:08 +0900

    Add UI test.

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from '../App';
-import { render, screen } from '@testing-library/react';
+import App, { getFormattedPrice } from '../App';
+import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom'
 
 /**
@@ -15,8 +15,27 @@ it('renders without crashing', () => {
   ReactDOM.render(<App />, div);
 });
 
-//
+//　チェックボックスのテスト
 it('renders select toppings message', () => {
   render(<App />);
   expect(screen.getByText('Select Toppings')).toBeInTheDocument();
+
+  // index: 2 のチェックボックスの状態確認
+  const targetCheckBox = screen.getByRole('checkbox', { name: 'Onions' });
+  expect(targetCheckBox).not.toBeChecked();
+
+  // クリック
+  fireEvent.click(targetCheckBox);
+
+  // 合計値をチェック
+  const totalPrice = screen.getByText((content, element) => {
+    return element.id == 'totalPrice' && content.startsWith('$3.00');
+  })
+
+  // $3.00 の表示を想定
+  expect(totalPrice).toBeInTheDocument;
+});
+
+it('return text with expected format', () => {
+  expect(getFormattedPrice(1.199)).toEqual('$1.20');
 });
